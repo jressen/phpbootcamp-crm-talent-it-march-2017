54a48534d3990612e8a51d5b4d5660836dc7b2a9
Michelangelo van Dam <michelangelo@in2it.be>
2017-02-10 11:02:24 +0100

    Setting up Auth module
    Setting up the basis for authentication

---
+++ module/Auth/config/module.config.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Auth;
+
+use Zend\Router\Http\Literal;
+
+return [
+    'routes' => [
+        'router' => [
+            'auth' => [
+                'type' => Literal::class,
+                'options' => [
+                    'route' => '/auth',
+                    'defaults' => [
+                        'controller' => '',
+                        'action' => 'index',
+                    ],
+                ],
+            ],
+        ],
+    ],
+    'view_manager' => [
+        'template_path_stack' => [
+            'contact' => __DIR__ . '/../view',
+        ],
+    ],
+];
---
+++ module/Auth/src/Controller/AuthController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Auth\Controller;
+
+
+use Zend\Mvc\Controller\AbstractActionController;
+
+class AuthController extends AbstractActionController
+{
+    public function indexAction()
+    {
+
+    }
+}
