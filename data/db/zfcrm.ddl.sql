daa9288f8256807442e1ccbc4be57b16a43835de
Michelangelo van Dam <michelangelo@in2it.be>
2017-03-15 14:19:28 +0100

    Updating database schema
    Here's the updated database structures

--- data/db/zfcrm.ddl.sql
+++ data/db/zfcrm.ddl.sql
@@ -1,69 +1,150 @@
-DROP TABLE IF EXISTS `member`;
-CREATE TABLE `member` (
-  `member_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `linkedin_id` VARCHAR(12) NOT NULL DEFAULT '',
-  `access_token` VARCHAR(200) NOT NULL DEFAULT '',
-  `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-  `modified` TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00',
-  PRIMARY KEY (`member_id`),
-  UNIQUE KEY `member_linkedin_uk` (`linkedin_id`)
-) Engine=InnoDb CHARACTER SET 'utf8' COLLATE 'utf8_general_ci';
+-- MySQL dump 10.13  Distrib 5.7.16, for osx10.11 (x86_64)
+--
+-- Host: localhost    Database: phpbootcamp_zfcrm
+-- ------------------------------------------------------
+-- Server version	5.7.16
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `contact`
+--
 
 DROP TABLE IF EXISTS `contact`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `contact` (
-  `contact_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `member_id` INT UNSIGNED NOT NULL,
-  `first_name` VARCHAR(150) NOT NULL DEFAULT '',
-  `last_name` VARCHAR(250) NOT NULL DEFAULT '',
-  `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-  `modified` TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `contact_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `member_id` int(10) unsigned NOT NULL,
+  `first_name` varchar(150) NOT NULL DEFAULT '',
+  `last_name` varchar(250) NOT NULL DEFAULT '',
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `modified` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`contact_id`),
-  INDEX `member_idx` (`member_id`)
-) Engine=InnoDb CHARACTER SET 'utf8' COLLATE 'utf8_general_ci';
+  KEY `member_idx` (`member_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-DROP TABLE IF EXISTS `contact_email`;
-CREATE TABLE `contact_email` (
-  `contact_email_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `member_id` INT UNSIGNED NOT NULL,
-  `contact_id` INT UNSIGNED NOT NULL,
-  `email_address` VARCHAR(250) NOT NULL,
-  `primary` TINYINT(1) NOT NULL DEFAULT 0,
-  `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-  `modified` TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00',
-  PRIMARY KEY (`contact_email_id`),
-  INDEX `member_idx` (`member_id`),
-  INDEX `contact_id_idx` (`contact_id`),
-  UNIQUE KEY `contact_email_uk` (`email_address`)
-) Engine=InnoDb CHARACTER SET 'utf8' COLLATE 'utf8_general_ci';
+--
+-- Table structure for table `contact_address`
+--
 
 DROP TABLE IF EXISTS `contact_address`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `contact_address` (
-  `contact_address_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `member_id` INT UNSIGNED NOT NULL,
-  `contact_id` INT UNSIGNED NOT NULL,
-  `street_1` VARCHAR(250) NOT NULL DEFAULT '',
-  `street_2` VARCHAR(250) NOT NULL DEFAULT '',
-  `postcode` VARCHAR(10) NOT NULL DEFAULT '',
-  `city` VARCHAR(250) NOT NULL DEFAULT '',
-  `province` VARCHAR(250) NOT NULL DEFAULT '',
-  `country_code` CHAR(2) NOT NULL DEFAULT '',
-  `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-  `modified` TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `contact_address_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `member_id` int(10) unsigned NOT NULL,
+  `contact_id` int(10) unsigned NOT NULL,
+  `street_1` varchar(250) NOT NULL DEFAULT '',
+  `street_2` varchar(250) NOT NULL DEFAULT '',
+  `postcode` varchar(10) NOT NULL DEFAULT '',
+  `city` varchar(250) NOT NULL DEFAULT '',
+  `province` varchar(250) NOT NULL DEFAULT '',
+  `country_code` char(2) NOT NULL DEFAULT '',
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `modified` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`contact_address_id`),
-  INDEX `member_idx` (`member_id`),
-  INDEX `contact_id_idx` (`contact_id`)
-) Engine=InnoDb CHARACTER SET 'utf8' COLLATE 'utf8_general_ci';
+  KEY `member_idx` (`member_id`),
+  KEY `contact_id_idx` (`contact_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `contact_email`
+--
+
+DROP TABLE IF EXISTS `contact_email`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `contact_email` (
+  `contact_email_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `member_id` int(10) unsigned NOT NULL,
+  `contact_id` int(10) unsigned NOT NULL,
+  `email_address` varchar(250) NOT NULL,
+  `primary` tinyint(1) NOT NULL DEFAULT '0',
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `modified` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`contact_email_id`),
+  UNIQUE KEY `contact_email_uk` (`email_address`),
+  KEY `member_idx` (`member_id`),
+  KEY `contact_id_idx` (`contact_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `contact_image`
+--
 
 DROP TABLE IF EXISTS `contact_image`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `contact_image` (
-  `contact_image_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `member_id` INT UNSIGNED NOT NULL,
-  `contact_id` INT UNSIGNED NOT NULL,
-  `image_link` TEXT NOT NULL,
-  `image_active` TINYINT(1) NOT NULL DEFAULT 0,
-  `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
-  `modified` TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `contact_image_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `member_id` int(10) unsigned NOT NULL,
+  `contact_id` int(10) unsigned NOT NULL,
+  `image_link` text NOT NULL,
+  `image_active` tinyint(1) NOT NULL DEFAULT '0',
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `modified` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`contact_image_id`),
-  INDEX `member_idx` (`member_id`),
-  INDEX `contact_id_idx` (`contact_id`)
-) Engine=InnoDb CHARACTER SET 'utf8' COLLATE 'utf8_general_ci';
+  KEY `member_idx` (`member_id`),
+  KEY `contact_id_idx` (`contact_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `country`
+--
+
+DROP TABLE IF EXISTS `country`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `country` (
+  `iso` char(2) NOT NULL,
+  `name` varchar(80) NOT NULL,
+  `nicename` varchar(80) NOT NULL,
+  `iso3` char(3) DEFAULT NULL,
+  `numcode` smallint(6) DEFAULT NULL,
+  `phonecode` int(5) NOT NULL,
+  PRIMARY KEY (`iso`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `member`
+--
+
+DROP TABLE IF EXISTS `member`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `member` (
+  `member_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `linkedin_id` varchar(12) NOT NULL DEFAULT '',
+  `access_token` varchar(200) NOT NULL DEFAULT '',
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `modified` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`member_id`),
+  UNIQUE KEY `member_linkedin_uk` (`linkedin_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-03-15 14:18:43
