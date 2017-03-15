c51997c3f8636660af12f3dbf08eaeb423013358
Michelangelo van Dam <michelangelo@in2it.be>
2017-03-15 14:05:19 +0100

    Changing the class names here too
    Authentication not possible without it

--- module/Auth/src/Service/Factory/MemberServiceFactory.php
+++ module/Auth/src/Service/Factory/MemberServiceFactory.php
@@ -6,9 +6,9 @@ namespace Auth\Service\Factory;
 use Auth\Entity\MemberEntity;
 use Auth\Service\MemberService;
 use Contact\Entity\Contact;
-use Contact\Entity\ContactAddress;
-use Contact\Entity\ContactEmail;
-use Contact\Entity\ContactImage;
+use Contact\Entity\Address;
+use Contact\Entity\EmailAddress;
+use Contact\Entity\Image;
 use Interop\Container\ContainerInterface;
 use Interop\Container\Exception\ContainerException;
 use Zend\ServiceManager\Exception\ServiceNotCreatedException;
@@ -28,11 +28,11 @@ class MemberServiceFactory implements FactoryInterface
             $container->get(\Contact\Model\ContactModelInterface::class),
             new Contact(0, 0, '', ''),
             $container->get(\Contact\Model\EmailAddressModelInterface::class),
-            new ContactEmail(0, 0, 0, ''),
+            new EmailAddress(0, 0, 0, ''),
             $container->get(\Contact\Model\AddressModelInterface::class),
-            new ContactAddress(0, 0, 0),
+            new Address(0, 0, 0),
             $container->get(\Contact\Model\ImageModelInterface::class),
-            new ContactImage(0, 0, 0, '')
+            new Image(0, 0, 0, '')
         );
     }
 
